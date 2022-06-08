7df6504a310a833950c2d40ddd972a68d247e5eb
zwgmy <36669321+zwgmy@users.noreply.github.com>
2022-06-08 14:34:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,16 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+
+echo 'src-git Sagit_Packages https://github.com/Sagit-chu/packages.git;small-package' >> feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git packages1 https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+#echo 'src-git packages1 https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #src-git luci https://github.com/coolsnowwolf/luci
 #src-git routing https://git.openwrt.org/feed/routing.git
 #src-git telephony https://git.openwrt.org/feed/telephony.git
