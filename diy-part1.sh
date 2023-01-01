1ad4dc7d0b176def018878e408146194818facb3
zwgmy <36669321+zwgmy@users.noreply.github.com>
2023-01-01 08:09:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,9 @@
 
 # Add a feed source
 
-echo 'src-git Sagit_Packages https://github.com/Sagit-chu/packages.git;small-package' >> feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git Sagit_Packages https://github.com/Sagit-chu/packages.git;small-package' >> feeds.conf.default
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
